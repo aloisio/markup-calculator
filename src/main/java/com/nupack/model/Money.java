9a81cd2d50682baeba0fa2b8e9313d402984474b
Aloisio Ribeiro <aloisio@users.noreply.github.com>
2015-02-19 19:36:27 -0500

    AR: Refactor method to convert Money to cents for clarity

--- src/main/java/com/nupack/model/Money.java
+++ src/main/java/com/nupack/model/Money.java
@@ -3,6 +3,8 @@ package com.nupack.model;
 import java.math.BigDecimal;
 
 public class Money {
+    private static final BigDecimal ONE_HUNDRED = BigDecimal.valueOf(100);
+    
     private final BigDecimal amount;
 
     public Money(double amountAsDouble) {
@@ -10,6 +12,10 @@ public class Money {
     }
 
     public long getCents() {
-        return BigDecimal.valueOf(100).multiply(amount).setScale(0, BigDecimal.ROUND_HALF_UP).longValue();
+        return roundToLong(ONE_HUNDRED.multiply(amount));
+    }
+
+    private long roundToLong(BigDecimal value) {
+        return value.setScale(0, BigDecimal.ROUND_HALF_UP).longValue();
     }
 }
